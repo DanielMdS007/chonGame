9fa27d0d73df900954fffbba2d228f3467d78fb8
DanielMdS007 <danielmotads007@gmail.com>
2025-04-14 16:39:34 +0200

    erradi

---
+++ game/src/main/java/chon/group/game/domain/environment/Environment.java
@@ -0,0 +1,293 @@
+package chon.group.game.domain.environment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import chon.group.game.domain.agent.Agent;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+/**
+ * Represents the game environment, including properties such as dimensions,
+ * position,
+ * background image, agents, and the protagonist.
+ * The environment also controls rendering, restricts the environment area,
+ * prints an agent's coordinates, and detects collisions between the protagonist
+ * and agents.
+ */
+public class Environment {
+
+    /** The X (horizontal) position of the environment. */
+    private int posX;
+
+    /** The Y (vertical) position of the environment. */
+    private int posY;
+
+    /** The width of the environment. */
+    private int width;
+
+    /** The height of the environment. */
+    private int height;
+
+    /** The background image of the environment. */
+    private Image image;
+
+    /** The protagonist in the environment. */
+    private Agent protagonist;
+
+    /** List of agents present in the environment. */
+    private List<Agent> agents = new ArrayList<Agent>();
+
+    /** The graphics context used to render the environment. */
+    private GraphicsContext gc;
+
+    /**
+     * Default constructor to create an empty environment.
+     */
+    public Environment() {
+    }
+
+    /**
+     * Constructor to initialize the environment with dimensions, position, and a
+     * background image.
+     *
+     * @param posX      the initial X (horizontal) position of the environment
+     * @param posY      the initial Y (vertical) position of the environment
+     * @param width     the width of the environment
+     * @param height    the height of the environment
+     * @param pathImage the path to the background image
+     */
+    public Environment(int posX, int posY, int width, int height, String pathImage) {
+        this.posX = posX;
+        this.posY = posY;
+        this.height = height;
+        this.width = width;
+        this.setImage(pathImage);
+        this.agents = new ArrayList<Agent>();
+    }
+
+    /**
+     * Constructor to initialize the environment with dimensions, position, a
+     * background image, and a list of agents.
+     *
+     * @param posX      the initial X (horizontal) position of the environment
+     * @param posY      the initial Y (vertical) position of the environment
+     * @param width     the width of the environment
+     * @param height    the height of the environment
+     * @param pathImage the path to the background image
+     * @param agents    the list of agents in the environment
+     */
+    public Environment(int posX, int posY, int width, int height, String pathImage, ArrayList<Agent> agents) {
+        this.posX = posX;
+        this.posY = posY;
+        this.height = height;
+        this.width = width;
+        this.setImage(pathImage);
+        this.setAgents(agents);
+    }
+
+    /**
+     * Gets the X (horizontal) position of the environment.
+     *
+     * @return the X position of the environment
+     */
+    public int getPosX() {
+        return posX;
+    }
+
+    /**
+     * Sets the X (horizontal) position of the environment.
+     *
+     * @param posX the new X position of the environment
+     */
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    /**
+     * Gets the Y (vertical) position of the environment.
+     *
+     * @return the Y position of the environment
+     */
+    public int getPosY() {
+        return posY;
+    }
+
+    /**
+     * Sets the Y (vertical) position of the environment.
+     *
+     * @param posY the new Y position of the environment
+     */
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    /**
+     * Gets the width of the environment.
+     *
+     * @return the width of the environment
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * Sets the width of the environment.
+     *
+     * @param width the new width of the environment
+     */
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    /**
+     * Gets the height of the environment.
+     *
+     * @return the height of the environment
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Sets the height of the environment.
+     *
+     * @param height the new height of the environment
+     */
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    /**
+     * Gets the background image of the environment.
+     *
+     * @return the background image of the environment
+     */
+    public Image getImage() {
+        return image;
+    }
+
+    /**
+     * Sets the background image of the environment.
+     *
+     * @param pathImage the path to the new background image
+     */
+    public void setImage(String pathImage) {
+        this.image = new Image(getClass().getResource(pathImage).toExternalForm());
+    }
+
+    /**
+     * Gets the protagonist of the environment.
+     *
+     * @return the protagonist of the environment
+     */
+    public Agent getProtagonist() {
+        return protagonist;
+    }
+
+    /**
+     * Sets the protagonist of the environment.
+     *
+     * @param protagonist the new protagonist of the environment
+     */
+    public void setProtagonist(Agent protagonist) {
+        this.protagonist = protagonist;
+    }
+
+    /**
+     * Gets the list of agents present in the environment.
+     *
+     * @return the list of agents
+     */
+    public List<Agent> getAgents() {
+        return agents;
+    }
+
+    /**
+     * Sets the list of agents present in the environment.
+     *
+     * @param agents the new list of agents
+     */
+    public void setAgents(ArrayList<Agent> agents) {
+        this.agents = agents;
+    }
+
+    /**
+     * Gets the graphics context used to render the environment.
+     *
+     * @return the graphics context
+     */
+    public GraphicsContext getGc() {
+        return gc;
+    }
+
+    /**
+     * Sets the graphics context used to render the environment.
+     *
+     * @param gc the new graphics context
+     */
+    public void setGc(GraphicsContext gc) {
+        this.gc = gc;
+    }
+
+    /**
+     * Draws the environment's background on the graphics context.
+     */
+    public void drawBackground() {
+        gc.drawImage(this.image, this.posX, this.posY, this.width, this.height);
+    }
+
+    /**
+     * Renders all agents and the protagonist in the environment.
+     */
+    public void drawAgents() {
+        for (Agent agent : this.agents) {
+            gc.drawImage(agent.getImage(), agent.getPosX(), agent.getPosY(), agent.getWidth(), agent.getHeight());
+        }
+        gc.drawImage(this.protagonist.getImage(), this.protagonist.getPosX(), this.protagonist.getPosY(),
+                this.protagonist.getWidth(), this.protagonist.getHeight());
+        printStatusPanel(this.protagonist);
+    }
+
+    /**
+     * Clears the environment area, removing previously drawn elements.
+     */
+    public void clearEnvironment() {
+        gc.clearRect(0, 0, this.width, this.height);
+    }
+
+    /**
+     * Displays a status panel showing the protagonist's coordinates.
+     *
+     * @param agent the protagonist whose information will be displayed
+     */
+    public void printStatusPanel(Agent agent) {
+        gc.setFill(Color.BLACK);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(2);
+        Font theFont = Font.font("Verdana", FontWeight.BOLD, 14);
+        gc.setFont(theFont);
+        gc.fillText("X: " + agent.getPosX(), agent.getPosX() + 10, agent.getPosY() - 25);
+        gc.fillText("Y: " + agent.getPosY(), agent.getPosX() + 10, agent.getPosY() - 10);
+    }
+
+    /**
+     * Checks if the protagonist is within the environment's boundaries and adjusts
+     * its position if necessary.
+     */
+    public void checkBorders() {
+        if (this.protagonist.getPosX() < 0) {
+            this.protagonist.setPosX(0);
+        } else if ((this.protagonist.getPosX() + this.protagonist.getWidth()) > this.width) {
+            this.protagonist.setPosX(this.width - protagonist.getWidth());
+        } else if (this.protagonist.getPosY() < 0) {
+            this.protagonist.setPosY(0);
+        } else if ((this.protagonist.getPosY() + this.protagonist.getHeight()) > this.height) {
+            this.protagonist.setPosY(this.height - this.protagonist.getHeight());
+        }
+    }
+
+}
